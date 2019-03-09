cfcf685902f3cb90870779d78ac2f83305e3210d
Tom Smith <tom.smith@okta.com>
2019-03-09 18:25:09 -0500

    initial commit

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const bodyParser = require('body-parser')
 
 const express = require('express')
 
+var session = require('express-session')
+
 const fs = require('fs')
 
 const OktaAuth = require('@okta/okta-auth-js')
+
+const okta = require('@okta/okta-sdk-nodejs')
+
+var morgan = require('morgan')
 
 const request = require('request')
 
@@ -20,18 +26,29 @@ const app = express()
 var port = process.env.PORT
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static('public'))
+
+app.use(morgan('combined'))
+
+app.use(session({ secret: process.env.SECRET_PHRASE, cookie: { maxAge: 60000 }}))
 
 app.listen(port, function () {
 	console.log('App listening on port ' + port + '...');
 })
 
-var config = {
-	url: process.env.OKTA_TENANT
-}
+///////////////////////////////////////////////////
 
-var authClient = new OktaAuth(config)
+// Okta clients
+
+const authClient = new OktaAuth({ url: process.env.OKTA_TENANT })
+
+const client = new okta.Client({
+	orgUrl: process.env.OKTA_TENANT,
+	token: process.env.OKTA_API_TOKEN,
+	requestExecutor: new okta.DefaultRequestExecutor() // Will be added by default in 2.0
+});
 
 //////////////////////////////////////////////////
 
@@ -42,99 +59,124 @@ app.get('/', function (req, res) {
 			console.log("error reading the index.html file")
 		}
 
-		page = page.replace(/{{ARKOSE_PUBLIC_KEY}}/g, process.env.ARKOSE_PUBLIC_KEY)
 		page = page.replace(/{{OKTA_TENANT}}/g, process.env.OKTA_TENANT)
 		page = page.replace(/{{APP_HOME}}/g, process.env.APP_HOME)
 
-		// this is just a little hack to show an example username/password on the home
-		// page of the public demo
-		if (process.env.CREDS) {
-			page = page.replace(/{{CREDS}}/g, process.env.CREDS)
-		}
-		else {
-			page = page.replace(/{{CREDS}}/g, "")
-		}
 		res.send(page)
 	})
 })
 
-app.post('/authn', function (req, res) {
+app.get('/schedule', function (req, res) {
+
+	if (req.session.authn === 1) {
+		res.send("you are authenticated!")
+	}
+
+	else {
+		res.send("you are NOT authenticated!")
+	}
+
+	// fs.readFile('html/index.html', "utf8", (err, page) => {
+	// 	if (err) {
+	// 		console.log("error reading the index.html file")
+	// 	}
+
+	// 	page = page.replace(/{{OKTA_TENANT}}/g, process.env.OKTA_TENANT)
+	// 	page = page.replace(/{{APP_HOME}}/g, process.env.APP_HOME)
+
+	// 	res.send(page)
+	// })
+})
+
+app.get('/log_in', function (req, res) {
+
+	fs.readFile('html/log_in.html', "utf8", (err, page) => {
+		if (err) {
+			console.log("error reading the index.html file")
+		}
+
+		page = page.replace(/{{OKTA_TENANT}}/g, process.env.OKTA_TENANT)
+		page = page.replace(/{{APP_HOME}}/g, process.env.APP_HOME)
+
+		res.send(page)
+	})
+
+	// fs.readFile('html/index.html', "utf8", (err, page) => {
+	// 	if (err) {
+	// 		console.log("error reading the index.html file")
+	// 	}
+
+	// 	page = page.replace(/{{OKTA_TENANT}}/g, process.env.OKTA_TENANT)
+	// 	page = page.replace(/{{APP_HOME}}/g, process.env.APP_HOME)
+
+	// 	res.send(page)
+	// })
+})
+
+app.post('/log_out', function (req, res) {
+
+	req.session.authn = 0
+
+	req.session.user_id = ""
+
+	console.log("killed the server-side session.")
+
+	res.json({msg: "success"})
+
+})
+
+app.post('/register', function (req, res) {
 
 	console.log("the request body is: ")
 
 	console.dir(req.body)
 
-	var err = {
-		errorCode: "E0000004",
-		errorSummary: "Authentication failed",
-		errorLink: "E0000004",
-		errorId: "",
-		errorCauses: []
-	}
-
-	// Check for the context token
-	// but don't redeem the context token until after Okta authn
-	// if user mis-types password they can still use the same context token
-
-	if (!req.body.contextData) {
-		err.errorCauses.push("No context token included.")
-		res.status(401).json(err)
-		return
-	}
-
-	authClient.signIn({
-		username: req.body.username,
-		password: req.body.password
-	})
-	.then(function(transaction) {
-		if (transaction.status === 'SUCCESS') {
-			console.log("the transaction is: ")
-			console.dir(transaction)
-
-			console.log("the sessionToken is: " + transaction.sessionToken)
-
-			var contextToken = req.body.contextData
-
-			redeemContextToken(contextToken, function(error, response) {
-
-				if (!response.solved) {
-					err.errorCauses.push("Context token did not pass inspection.")
-					res.status(401).json(err)
-					return
-				}
-
-				res.json(transaction)
-			})
-		} else {
-			throw 'We cannot handle the ' + transaction.status + ' status';
+	const newUser = {
+		profile: {
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			email: req.body.email,
+			login: req.body.email
+		},
+		credentials: {
+			password : {
+				value: req.body.password
+			}
 		}
-	})
-	.fail(function(err) {
-		console.error(err);
+	}
+
+	client.createUser(newUser)
+	.then(user => {
+		console.log('Created user', user)
+
+		authClient.signIn({
+			username: req.body.email,
+			password: req.body.password
+		})
+		.then(function(transaction) {
+			if (transaction.status === 'SUCCESS') {
+				console.log("the transaction is: ")
+				console.dir(transaction)
+
+				console.log("the sessionToken is: " + transaction.sessionToken)
+
+				var session_cookie_uri = process.env.OKTA_TENANT + "/login/sessionCookieRedirect?token=" + transaction.sessionToken + "&redirectUrl=" + process.env.APP_HOME
+
+				req.session.authn = 1
+				req.session.user_id = transaction.user.id
+
+				console.log("the value of req.session is: " + req.session.authn)
+
+				console.log("the user id is: " + req.session.user_id)
+
+				res.redirect(session_cookie_uri)
+
+			} else {
+				throw 'We cannot handle the ' + transaction.status + ' status';
+			}
+		})
+		.fail(function(err) {
+			console.error(err);
+		})
 	})
 })
-
-function redeemContextToken(contextToken, callback) {
-
-	var url = "https://verify.arkoselabs.com/fc/v/"
-	url += "?private_key=" + process.env.ARKOSE_PRIVATE_KEY
-	url += "&session_token=" + contextToken
-
-	var options = {
-		method: 'GET',
-		url: url,
-		headers: {
-			'cache-control': 'no-cache',
-			'Content-Type': 'application/json',
-			Accept: 'application/json'
-		}
-	}
-
-	request(options, function (error, response, body) {
-		if (error) throw new Error(error)
-
-		console.log(body)
-
-		return callback(null, JSON.parse(body))
-	})
-}
