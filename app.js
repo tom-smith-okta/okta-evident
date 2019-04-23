f23ca56c6eedfd558234827c2990ee7cc972d55f
Tom Smith <tom.smith@okta.com>
2019-04-23 16:04:22 -0400

    release candidate

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const express = require('express')
 
 const fs = require('fs')
 
-// var morgan = require('morgan')
-
 const request = require('request')
 
 ///////////////////////////////////////////////////
@@ -53,6 +51,78 @@ app.get('/', function (req, res) {
 	})
 })
 
+// to-do: re-use code between the "poll" and "records"
+// endpoints
+// or use a server-side session
+app.post('/poll', function (req, res) {
+
+	console.log("the request body is: ")
+	console.dir(req.body)
+
+	var options = {
+		method: 'POST',
+		url: process.env.OKTA_TENANT + '/oauth2/v1/introspect',
+		qs: {
+			token: req.body.id_token,
+			token_type_hint: 'id_token',
+			client_id: process.env.CLIENT_ID,
+			client_secret: process.env.CLIENT_SECRET
+		},
+		headers: {
+			'cache-control': 'no-cache',
+			'Content-Type': 'application/x-www-form-urlencoded',
+			Accept: 'application/json'
+		}
+	}
+
+	request(options, function (error, response, body) {
+		if (error) throw new Error(error)
+
+		console.log(body)
+
+		var obj = JSON.parse(body)
+
+		if (obj.active === true) {
+			console.log("the id token is active.")
+
+			console.dir(obj)
+
+			var user_id = obj.sub
+
+			console.log("the user id (sub) is: " + user_id)
+
+			var options = {
+				method: 'GET',
+				url: process.env.OKTA_TENANT + '/api/v1/users/' + user_id,
+				headers: {
+					'cache-control': 'no-cache',
+					Authorization: 'SSWS ' + process.env.API_TOKEN,
+					'Content-Type': 'application/json',
+					Accept: 'application/json'
+				}
+			}
+
+			request(options, function (error, response, body) {
+				if (error) throw new Error(error)
+
+				console.log(body)
+
+				var obj = JSON.parse(body)
+
+				console.log("__________________________________")
+
+				if (obj.profile.evidentid_verification) {
+					console.log("the value of evidentid_verification is: " + obj.profile.evidentid_verification)
+
+					res.json({status: true})
+					return
+				}
+				res.json({status: false})
+			})
+		}
+	})
+})
+
 app.post('/records', function (req, res) {
 
 	console.log("the request body is: ")
@@ -88,18 +158,39 @@ app.post('/records', function (req, res) {
 
 			console.dir(obj)
 
-			res.json({html: "<img src='" + records_img_url + "'>"})
+			var user_id = obj.sub
 
-			return
+			console.log("the user id (sub) is: " + user_id)
 
-			// if (obj.evidentid_verification) {
-			// 	if (obj.evidentid_verification === true) {
-			// 		res.json({html: "<img src='" + records_img_url + "'>"})
-			// 		return
-			// 	}
-			// }
+			var options = {
+				method: 'GET',
+				url: process.env.OKTA_TENANT + '/api/v1/users/' + user_id,
+				headers: {
+					'cache-control': 'no-cache',
+					Authorization: 'SSWS ' + process.env.API_TOKEN,
+					'Content-Type': 'application/json',
+					Accept: 'application/json'
+				}
+			}
+
+			request(options, function (error, response, body) {
+				if (error) throw new Error(error)
+
+				console.log(body)
+
+				var obj = JSON.parse(body)
+
+				console.log("__________________________________")
+
+				if (obj.profile.evidentid_verification) {
+					console.log("the value of evidentid_verification is: " + obj.profile.evidentid_verification)
+
+					res.json({html: "<img src='" + records_img_url + "'>"})
+					return
+				}
+				res.json({html: errMsg})
+			})
 		}
-		res.json({html: errMsg})
 	})
 })
 
